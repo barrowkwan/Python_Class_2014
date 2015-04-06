a9364844b12d1c19cb2fd50909793b3f300c972b
Truckboy <wong.ian.yc@gmail.com>
2015-04-05 19:45:40 -0700

    Bank Project
    Bank

--- bank.py
+++ bank.py
@@ -2,7 +2,15 @@
 #  Bank Program
 #
 def withdraw():
-    print "I am withdrawing"
+    w_money = int(raw_input("How much money do you want to withdraw?"))
+    global balance
+    if balance < 0:
+        print "that balance is not valid"
+    elif balance > 0:
+        my_book = open(bank_book,'w')
+        my_book.write (str(balance - w_money))
+        my_book.close()
+        balance = balance - w_money
 
 def deposit():
     print "This old machine is now depositing your money$$........"
