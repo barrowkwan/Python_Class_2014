a550fedcfe7c296d1f8847b85b58a9e9bd9f743d
bellachip49 <krystal.m.kwan@gmail.com>
2015-07-11 15:23:50 -0700

    modify excercise1.py

--- krystal/exercise1.py
+++ krystal/exercise1.py
@@ -7,13 +7,10 @@ script_name, num_1, num_2 = argv
 num_1 = int(num_1)
 num_2 = int(num_2)
 
-def max(largest_number):
-    print largest_number
-
-
-if num_1 > num_2:
-    largest_number = num_1
-    max(largest_number)
-else:
-    largest_number = num_2
-    max(largest_number)
+def max():
+    global num_1
+    global num_2
+    if num_1 > num_2:
+        largest_number = num_1
+    else:
+        largest_number = num_2
