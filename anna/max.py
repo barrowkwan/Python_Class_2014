a02a71cbff9d1c71f06ca9a4d5724c7b9c5c6a50
Anna Lee <annalee.123.123@gmail.com>
2015-07-11 15:46:51 -0700

    max revised

--- anna/max.py
+++ anna/max.py
@@ -10,8 +10,10 @@ num_2 = int(raw_input("Second number:"))
 def max(num_1, num_2):
     if num_1 > num_2:
         print num_1
+        return num_1
     else:
         print num_2
+        return num_2
 
 max(num_1, num_2)
 
