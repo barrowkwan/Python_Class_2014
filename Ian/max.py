47ba256afe14d11fd5d607ce7e5e0b554bc82ddd
Ian Wong <wong.ian.yc@gmail.com>
2015-07-11 15:46:59 -0700

    midify max.py

--- Ian/max.py
+++ Ian/max.py
@@ -8,8 +8,10 @@ num2=(raw_input("Number 2:"))
 
 def max(num1,num2):
     if num1>num2:
-        print "num1"
+        print num1
+        return num1
     if num2>num1:
-        print "num 2"
+        print num2
+        return num2
 
 max(num1,num2)
