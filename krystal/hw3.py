cf95add98919804012d7313c4b233a501851d6b5
bellachip49 <krystal.m.kwan@gmail.com>
2015-07-23 14:44:15 -0700

    edit hw3.py

--- krystal/hw3.py
+++ krystal/hw3.py
@@ -4,22 +4,25 @@
 
 continuation = "y"
 
-while continuation == "y":
-    def triangle_area():
-        print "You picked triangle, what is the base of the triangle(cm)?"
-        b = raw_input("> ")
-        print "What is the height of the triangle(cm)?"
-        h = raw_input("> ")
-        
-    def rectangle_area():
-        print "You picked rectangle, what is the width of the rectangle(cm)?"
-        w = int(raw_input("> "))
-        print "What is the length of the rectangle(cm)?"
-        l = int(raw_input("> "))
-        area_of_rect = w * l
-        print "The area of the rectangle is %i" % (area_of_rect)
-        
+def triangle_area():
+    print "You picked triangle, what is the base of the triangle(cm)?"
+    b = int(raw_input("> "))
+    print "What is the height of the triangle(cm)?"
+    h = int(raw_input("> "))
+    area_of_tri = (b * h) / 2
+    print "The area of the triangle is %i" % (area_of_tri)
+    
+def rectangle_area():
+    print "You picked rectangle, what is the width of the rectangle(cm)?"
+    w = int(raw_input("> "))
+    print "What is the length of the rectangle(cm)?"
+    l = int(raw_input("> "))
+    area_of_rect = w * l
+    print "The area of the rectangle is %i" % (area_of_rect)
+    
 
+
+while continuation == "y":
     print "Please pick a shape:"
     print "\t 1) rectangle"
     print "\t 2) triangle"
@@ -28,8 +31,8 @@ while continuation == "y":
 
     if shape == "rectangle" or shape == "1":
         rectangle_area()
-    #elif shape == "triangle" or shape == "2":
-        #triangle_area() 
+    elif shape == "triangle" or shape == "2":
+        triangle_area() 
     else:
         print "Invalid choice."
 
