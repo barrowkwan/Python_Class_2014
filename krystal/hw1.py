f7f6dfa5b0ccc262226cbba21391b8e8dcba79ff
bellachip49 <krystal.m.kwan@gmail.com>
2015-08-15 15:53:05 -0700

    correct hw1.py

--- krystal/hw1.py
+++ krystal/hw1.py
@@ -16,5 +16,17 @@ num4 = int(num4)
 
 num5 = int(num5)
 
+def max(num_1, num_2):
+    if num_1 > num_2:
+        return num_1
+    else:
+        return num_2
+
+def min(num_1, num_2):
+    if num_1 > num_2:
+        return num_1
+    else:
+        return num_2
+
 print "Max: %i" % max(max(max(max(num1, num2), num3), num4), num5)
 print "Min: %i" % min(min(min(min(num1, num2), num3), num4), num5)
