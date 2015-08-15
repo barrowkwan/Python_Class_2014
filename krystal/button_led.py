1fc1e5553a18a6023263767bc8e61a1965323a7a
Krystal Kwan <krystal.m.kwan@gmail.com>
2015-08-15 15:04:46 -0700

    edit button_led.py

--- krystal/button_led.py
+++ krystal/button_led.py
@@ -5,7 +5,7 @@ import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4,GPIO.OUT)
-GPIO.setup(17, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(21, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 while True:
   if (GPIO.input(17) == 1):
