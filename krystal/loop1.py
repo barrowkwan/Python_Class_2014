86e4d5247f17cccce74ff29c1b1f89833b873442
bellachip49 <krystal.m.kwan@gmail.com>
2015-08-18 18:25:32 -0700

    correct loop1.py

--- krystal/loop1.py
+++ krystal/loop1.py
@@ -2,13 +2,13 @@
 # python
 # loops
 
-print "Number?"
-num = int(raw_input("> "))
-num = num + 1
-
 def count_up():
     global num
     for i in range(1, num, 1):
         print i
+
+print "Number?"
+num = int(raw_input("> "))
+num = num + 1
 
 count_up(num)
