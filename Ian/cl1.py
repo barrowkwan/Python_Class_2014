02c7dff22d47f0c4b645e90fbf0124505eccb9d5
Ian Wong <wong.ian.yc@gmail.com>
2015-09-05 15:45:16 -0700

    modify file

--- Ian/cl1.py
+++ Ian/cl1.py
@@ -13,3 +13,10 @@ a = "100"
 b = "200"
 
 print "Sum of a + b = %i" % (int(a)+int(b))
+
+name=str(raw_input("What is your name?"))
+born=int(raw_input("How old are you?"))
+print "%s, you are %i years old." % (name,born)
+print "10 years later you are %i years old" % (born+10)
+
+
