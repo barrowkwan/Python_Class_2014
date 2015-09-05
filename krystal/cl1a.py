cf91f74c97781f7d911624fc6c695e3fe7d64969
bellachip49 <krystal.m.kwan@gmail.com>
2015-09-05 16:34:22 -0700

    modify cl1a.py

--- krystal/cl1a.py
+++ krystal/cl1a.py
@@ -4,12 +4,12 @@
 # Date: 9/5/2015
 # Description: asks the name and tells you how many letters your name has
 
-def letter_name():
-    global name
+def letter_name(name):
     numbers = len(name)
     print "You have %i letters in your name." % (numbers)
 
 print "What is your name?"
 name = raw_input('> ')
 
-letter_name()
+
+letter_name(name)
