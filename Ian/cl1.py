69cdb9d89384f8e415b35dba6cf2ff5b565f698a
Ian Wong <wong.ian.yc@gmail.com>
2015-09-05 15:33:40 -0700

    make file

---
+++ Ian/cl1.py
@@ -0,0 +1,15 @@
+#!usr/bin/env python
+#Ian Wong
+#9/5/15
+#Printing
+
+print str("hello world")
+print 100
+x = "Hello World"
+y = 100
+print "%s : %i" % (x,y)
+
+a = "100"
+b = "200"
+
+print "Sum of a + b = %i" % (int(a)+int(b))
