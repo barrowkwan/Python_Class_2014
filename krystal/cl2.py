2d41484279d7f38b3252180ee6e3449b362bf52a
bellachip49 <krystal.m.kwan@gmail.com>
2015-09-12 16:22:46 -0700

    make cl2.py

---
+++ krystal/cl2.py
@@ -0,0 +1,11 @@
+#!usr/bin/env python
+# Name: Krystal Kwan
+# Date: 9/12/2015
+# Description: squares the number that the user passed in
+
+x = int(raw_input('Number: '))
+
+def squared_num(x):
+    return x * x
+
+print "Your squared number is %i." % (squared_num(x))
