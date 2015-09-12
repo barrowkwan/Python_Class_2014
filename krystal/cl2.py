7ab5ca4bdbb6cbc0faa526e157946f65ae944fab
bellachip49 <krystal.m.kwan@gmail.com>
2015-09-12 16:58:51 -0700

    make cl2.py

--- krystal/cl2.py
+++ krystal/cl2.py
@@ -4,8 +4,12 @@
 # Description: squares the number that the user passed in
 
 x = int(raw_input('Number: '))
+y = raw_input('String: ')
 
 def squared_num(x):
     return x * x
+def convert_string(y):
+    return (y[:1] + y [2:])
 
+convert_string(y)
 print "Your squared number is %i." % (squared_num(x))
