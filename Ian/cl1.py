2044b721b496f178f035c20690a0dc559ff38c48
Ian Wong <wong.ian.yc@gmail.com>
2015-09-12 15:22:34 -0700

    first and last name

--- Ian/cl1.py
+++ Ian/cl1.py
@@ -2,8 +2,13 @@
 #Ian Wong
 #9/5/15
 #Printing
-def letters():
-    name=raw_input("What is your name?")
-    print len(name)
- 
-letters()
+def letters(name,last):
+    full = name + last
+    return full
+
+name=raw_input("What is your first name?")
+last=raw_input("What is your last name?")
+    
+full_name = letters(name,last)
+
+print "Your name is %s" % (full_name)
