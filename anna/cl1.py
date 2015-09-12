29dfc6f7502f87509a32f0f4ea2f3a15362077e2
Anna Lee <annalee.123.123@gmail.com>
2015-09-12 14:58:51 -0700

    first and last name

--- anna/cl1.py
+++ anna/cl1.py
@@ -8,8 +8,8 @@ fn = raw_input('What is your first name?')
 ln = raw_input('What is your last name?')
 
 def name(fn,ln):
-   print 'Your name is full name is %s %s' % (fn, ln)
+ a = 'Your full name is %s %s' % (fn,ln)
+ print a
 
 name(fn,ln)
-    
-#ask first and last name return string that combines first and last name together
+
