0e2821ca68c77820ffea2c26944aecdfb60c1942
Ian Wong <ianwong@kiwi.attlocal.net>
2015-12-20 15:31:48 -0800

    Counting numbers file

---
+++ Ian/counting.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#Name:Ian Wong
+#Date:12/20/15
+#Assignment:Python Counting Program
+
+def one():
+	for i in range(1,11):
+		print (i)
+
+def two():
+	for i in range(2,20,2):
+		print (i)
+def three():
+	for i in range(21,0,-2):
+		print (i)
+
+one()
+two()
+three()
