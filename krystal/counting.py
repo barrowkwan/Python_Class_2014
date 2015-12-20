a753cbf5080950a837ecf620af9977e8f2a839ce
Krystal Kwan <krystalkwan@kiwi.attlocal.net>
2015-12-20 15:38:53 -0800

    make file

---
+++ krystal/counting.py
@@ -0,0 +1,20 @@
+# Name: Krystal Kwan
+# Date: 12/20/2015
+# Description: Python Counting Program
+#!/usr/bin/env python
+
+def function1():
+	for i in range (1,11):
+ 		print (i)
+
+def function2():
+	for i in range(2,21,2):
+		print (i)
+
+def function3():
+	for i in range(20,1,-2):
+		print (i)
+
+function1()
+function2()
+function3()
