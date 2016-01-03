22c7b6c3bd9f5154649a386ec18fa7370bb3c5d4
Ian Wong <ianwong@kiwi.attlocal.net>
2016-01-03 14:28:28 -0800

    Counting words

--- Ian/hw4.py
+++ Ian/hw4.py
@@ -1,24 +1,12 @@
-#Ian Wong
-#July 28,2015
-#Maximum and Minimum
-num1=int(raw_input("Number 1:"))
-num2=int(raw_input("Number 2:"))
-num3=int(raw_input("Number 3:"))
-num4=int(raw_input("Number 4:"))
-num5=int(raw_input("Number 5:"))
+#!usr/bin/env python
+#Name: Ian Wong
+#Date: 1/3/16
+#Assignment: Counting Words
 
-def max_num(x,y):
-    if x > y:
-        return x
-    else:
-        return y
-ma=max(max(max(max(num1,num2),num3),num4),num5)
-print "Maximum: %d" % ma
+string = input("What is your string? ")
 
-def min_num(x,y):
-    if x < y:
-        return x
-    else:
-        return y
-mi=min(min(min(min(num1,num2),num3),num4),num5)
-print "Minimum: %d" % mi
+count = string.count(' ')
+words = count + 1
+
+print("You have %i words in your string." % (words))
+
