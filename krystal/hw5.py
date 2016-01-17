1393a32816bc49a0f6e7ff42ffc2da7c4cf906ee
bellachip49 <krystal.m.kwan@gmail.com>
2016-01-17 12:57:55 -0800

    recover hw5.py

---
+++ krystal/hw5.py
@@ -0,0 +1,10 @@
+Name: Krystal Kwan
+# Date: 1/3/2016
+# Description: Change first letter of each word to uppercase
+#!/usr/bin/env python
+
+print ("String:")
+string = input("> ")
+
+string1 = string.lower()
+print (string1)
