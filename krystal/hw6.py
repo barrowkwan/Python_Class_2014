4701dbbf41b7407c5613873265e011a9305a3631
bellachip49 <krystal.m.kwan@gmail.com>
2016-01-17 13:06:58 -0800

    recover hw6.py

---
+++ krystal/hw6.py
@@ -0,0 +1,11 @@
+Name: Krystal Kwan
+# Date: 1/3/2016
+# Description: Count how many vowels in each string
+#!/usr/bin/env python
+
+print "String: "
+string = raw_input("> ")
+vowels = "aeiou"
+vowel1 = vowels.lower()
+for v in vowels:
+    print 
