9731ded4418d5c4ce4ced4ba0cc9ea1f01209625
bellachip49 <krystal.m.kwan@gmail.com>
2016-02-14 16:42:29 -0800

    make file

---
+++ krystal/problem1.py
@@ -0,0 +1,13 @@
+# Krystal Kwan
+# 2/14/2016
+# Make questions for a first grader
+from random import randint
+s = 0
+for i in range(1,11):
+    num1 = randint(1,99)
+    num2 = randint(1,99)
+    print "%i. %i + %i = " % (i, num1, num2)
+    ans = int(raw_input("> "))
+    if ans == num1 + num2:
+        s += 1
+print "You have %i out of 10 questions correct." % (s)
