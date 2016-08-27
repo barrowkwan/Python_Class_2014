c7508a3cf7e43d3c438abb4d7d97c43144e66b8d
Bella Chip <krystal.m.kwan@gmail.com>
2016-08-27 16:35:11 -0700

    make file

--- krystal/ex1.py
+++ krystal/ex1.py
@@ -5,10 +5,10 @@
 def sum(x, y):
     return x + y
 
-def mix(color1, color2)
+def mix(color1, color2):
     return "purple"
 
 
 print("7 + 5 = %i" % sum(7, 5))
 
-print("red + blue = %s" % mix("blue","red"))
+print("red + blue = %s" % mix("red", "blue"))
