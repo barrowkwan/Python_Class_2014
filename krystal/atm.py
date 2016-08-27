d00b66fd9c4206cf8aa8fdc1943dd379c5b28c6c
Bella Chip <krystal.m.kwan@gmail.com>
2016-08-27 16:16:31 -0700

    make virtual atm

--- krystal/atm.py
+++ krystal/atm.py
@@ -5,69 +5,74 @@
 import time
 import sys
 
-m = 0
 
-def withdraw():
-    global m
+
+def withdraw(m):
     try:
         print("Withdraw amount:")
         w = float(input("> "))
         if w > m:
-          print("Not enough!")
+            print("Not enough!")
         else:
-          print("Withdrawing $%.2f..." % w)
-          m -= w
-          time.sleep(2)
-          print("Done.")
+            print("Withdrawing $%.2f..." % w)
+            m -= w
+            time.sleep(2)
+            print("Done.")
+            return m
     except ValueError:
         print("Invalid Input.")
     except KeyboardInterrupt:
         print("Quitting...")
         sys.exit(0)
 
-def balance():
-    global m
+def balance(m):
     print("Your current balance: $%.2f." % m)
+    return m
 
-def deposit():
-        global m
-        try:
-            print("Deposit amount:")
-            w = float(input("> "))
-            if w <= 0:
-              print("Invalid Input.")
-            else:
-              print("Depositing $%.2f..." % w)
-              m += w
-              time.sleep(2)
-              print("Done.")
-        except ValueError:
+def deposit(m):
+    try:
+        print("Deposit amount:")
+        w = float(input("> "))
+        if w <= 0:
             print("Invalid Input.")
-        except KeyboardInterrupt:
-            print("Quitting...")
-            sys.exit(0)
+        else:
+            print("Depositing $%.2f..." % w)
+            m = m + w
+            time.sleep(2)
+            print("Done.")
+            return m
+    except ValueError:
+        print("Invalid Input.")
+    except KeyboardInterrupt:
+        print("Quitting...")
+        sys.exit(0)
 
-def decide(a):
+def decide(a, m):
     if a == 1:
-        balance()
+        return balance(m)
     elif a == 2:
-        deposit()
+        return deposit(m)
     elif a == 3:
-        withdraw()
+        return withdraw(m)
     else:
         print("Invalid Input.")
-try:
-    print("Your Name:")
-    n = input("> ")
-    print("Your current balance:")
-    b = float(input("> "))
-    m = b
-except ValueError:
-    print("Input Invalid.")
-except KeyboardInterrupt:
-    print("Quitting...")
 
+m = 0.0
 
+while True:
+    try:
+        print("Your Name:")
+        n = input("> ")
+        print("Your current balance:")
+        m = float(input("> "))
+        break
+    except ValueError:
+        print("Input Invalid.")
+    except KeyboardInterrupt:
+        sys.exit(0)
+    except NameError:
+        print("Input Invalid.")
+        continue
 
 while True:
     try:
@@ -76,7 +81,7 @@ while True:
         print("\t2) Deposit")
         print("\t3) Withdraw")
         a = float(input("> "))
-        decide(a)
+        m = decide(a, m)
     except ValueError:
         print("Input Invalid.")
     except KeyboardInterrupt:
